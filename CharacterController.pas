acde0049f6708201ee94a9550f9f5363f92ce196
k0lan4ik <kolasynnikolai@gmail.com>
2025-03-06 00:01:52 +0300

    Relocate to Lists

--- CharacterController.pas
+++ CharacterController.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
-  System.Classes, Vcl.Graphics,
+  System.Classes, Vcl.Graphics, EventController,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
 
 type
@@ -15,26 +15,35 @@ type
     Linked: array of TBodyPart; // указатель + кол частей
   end;
 
-  TChrKeyFrame = record
+  TChrKeyFrameInf = record
     Body: TBodyPart;
     Len: Integer;
     Time: Cardinal;
   end;
 
+  TChrKeyFrameAdr = ^TChrKeyFrame;
+
+  TChrKeyFrame = record
+    Inf: TChrKeyFrameInf;
+    Adr: TChrKeyFrameAdr;
+  end;
+
   TBodyPRow = array [1 .. 14, 1 .. 2] of Integer;
 
-  TCharacter = class
+  TCharacter = class(TDrawObject)
   public
-    KeyFrames: array of TChrKeyFrame;
+    KeyFrames: TChrKeyFrameAdr;
+    EndFrame: TChrKeyFrameAdr;
     Canvas: TCanvas;
     constructor Create(const Canvas: TCanvas);
-    procedure Paint(const CurrentTime: Cardinal);
+    procedure Paint(const CurrentTime: Cardinal); override;
 
     procedure AddKeyFrame(const Parts: TBodyPRow; const Len: Integer;
       const Time: Cardinal);
   private
-    procedure Draw(const left, right: TBodyPart;const LeftT, RightT, CurT: Cardinal;
- const CurAbs: TPoint;const Len: Integer);
+    procedure Draw(const left, right: TBodyPart;
+      const LeftT, RightT, CurT: Cardinal; const CurAbs: TPoint;
+      const Len: Integer);
     function CalculateEndPoint(Length: Integer; const Target: TPoint): TPoint;
   end;
 
@@ -43,9 +52,9 @@ implementation
 constructor TCharacter.Create(const Canvas: TCanvas);
 begin
   self.Canvas := Canvas;
-  self.Canvas.Pen.Width := 3;
-  self.Canvas.Pen.Color := clBlack;
-  self.Canvas.Brush.Color := clWhite;
+  New(self.KeyFrames);
+  self.KeyFrames^.Adr := nil;
+  self.EndFrame := self.KeyFrames;
   // SetLength(self.KeyFrames, 1);
 end;
 
@@ -62,118 +71,130 @@ end;
 procedure TCharacter.AddKeyFrame(const Parts: TBodyPRow; const Len: Integer;
   const Time: Cardinal);
 begin
-  SetLength(self.KeyFrames, Length(self.KeyFrames) + 1);
-  self.KeyFrames[High(self.KeyFrames)].Time := Time;
-  self.KeyFrames[High(self.KeyFrames)].Len := Len;
-  self.KeyFrames[High(self.KeyFrames)].Body.Local :=
-    Point(Parts[1, 1], Parts[1, 2]);
-  SetLength(self.KeyFrames[High(self.KeyFrames)].Body.Linked, 3);
-  with self.KeyFrames[High(self.KeyFrames)].Body do
-  begin
-    with Linked[0] do
+  New(self.EndFrame^.Adr);
+  self.EndFrame := self.EndFrame.Adr;
+  self.EndFrame^.Adr := nil;
+  with self.EndFrame^ do
+  begin;
+    Inf.Time := Time;
+    Inf.Len := Len;
+    Inf.Body.Local := Point(Parts[1, 1], Parts[1, 2]);
+    SetLength(Inf.Body.Linked, 3);
+    with Inf.Body do
     begin
-      Local := CalculateEndPoint(Len,
-        Point(Parts[2, 1], Parts[2, 2]));
-      SetLength(Linked, 1);
       with Linked[0] do
       begin
-
+        Local := CalculateEndPoint(Len, Point(Parts[2, 1], Parts[2, 2]));
         SetLength(Linked, 1);
-        Linked[0].Local := CalculateEndPoint(Len,
-          Point(Parts[3, 1], Parts[3, 2]));
         with Linked[0] do
         begin
-          SetLength(Linked, 3);
 
-
-          Linked[0].Local := CalculateEndPoint(Trunc(Len * 0.75),
-            Point(Parts[4, 1], Parts[4, 2]));
-
-          Linked[0].Local := CalculateEndPoint(Trunc(Len * 0.75),
-            Point(Parts[4, 1], Parts[4, 2]));
-
-          Linked[0].Local := CalculateEndPoint(Trunc(Len * 0.75),
-            Point(Parts[4, 1], Parts[4, 2]));
-          Linked[0].IsHead := true;
-
-          Linked[1].Local := CalculateEndPoint(Len,
-            Point(Parts[5, 1], Parts[5, 2]));
-          with Linked[1] do
+          SetLength(Linked, 1);
+          Linked[0].Local := CalculateEndPoint(Len,
+            Point(Parts[3, 1], Parts[3, 2]));
+          with Linked[0] do
           begin
-            SetLength(Linked, 1);
-            Linked[0].Local := CalculateEndPoint(Len,
-              Point(Parts[6, 1], Parts[6, 2]));
-          end;
+            SetLength(Linked, 3);
 
-          Linked[2].Local := CalculateEndPoint(Len,
-            Point(Parts[7, 1], Parts[7, 2]));
-          with Linked[2] do
-          begin
-            SetLength(Linked, 1);
-            Linked[0].Local := CalculateEndPoint(Len,
-              Point(Parts[8, 1], Parts[8, 2]));
+            Linked[0].Local := CalculateEndPoint(Trunc(Len * 0.75),
+              Point(Parts[4, 1], Parts[4, 2]));
+
+            Linked[0].Local := CalculateEndPoint(Trunc(Len * 0.75),
+              Point(Parts[4, 1], Parts[4, 2]));
+
+            Linked[0].Local := CalculateEndPoint(Trunc(Len * 0.75),
+              Point(Parts[4, 1], Parts[4, 2]));
+            Linked[0].IsHead := true;
+
+            Linked[1].Local := CalculateEndPoint(Len,
+              Point(Parts[5, 1], Parts[5, 2]));
+            with Linked[1] do
+            begin
+              SetLength(Linked, 1);
+              Linked[0].Local := CalculateEndPoint(Len,
+                Point(Parts[6, 1], Parts[6, 2]));
+            end;
+
+            Linked[2].Local := CalculateEndPoint(Len,
+              Point(Parts[7, 1], Parts[7, 2]));
+            with Linked[2] do
+            begin
+              SetLength(Linked, 1);
+              Linked[0].Local := CalculateEndPoint(Len,
+                Point(Parts[8, 1], Parts[8, 2]));
+            end;
           end;
         end;
       end;
-    end;
 
-    Linked[1].Local := CalculateEndPoint(Len, Point(Parts[9, 1], Parts[9, 2]));
-    with Linked[1] do
-    begin
-      SetLength(Linked, 1);
-      Linked[0].Local := CalculateEndPoint(Len,
-        Point(Parts[10, 1], Parts[10, 2]));
-      with Linked[0] do
+      Linked[1].Local := CalculateEndPoint(Len,
+        Point(Parts[9, 1], Parts[9, 2]));
+      with Linked[1] do
       begin
         SetLength(Linked, 1);
-        Linked[0].IsFoot := true;
-        Linked[0].Local := CalculateEndPoint(Trunc(Len * 0.5),
-          Point(Parts[11, 1], Parts[11, 2]));
+        Linked[0].Local := CalculateEndPoint(Len,
+          Point(Parts[10, 1], Parts[10, 2]));
+        with Linked[0] do
+        begin
+          SetLength(Linked, 1);
+          Linked[0].IsFoot := true;
+          Linked[0].Local := CalculateEndPoint(Trunc(Len * 0.5),
+            Point(Parts[11, 1], Parts[11, 2]));
+        end;
       end;
-    end;
 
-    Linked[2].Local := CalculateEndPoint(Len,
-      Point(Parts[12, 1], Parts[12, 2]));
-    with Linked[2] do
-    begin
-      SetLength(Linked, 1);
-      Linked[0].Local := CalculateEndPoint(Len,
-        Point(Parts[13, 1], Parts[13, 2]));
-      with Linked[0] do
+      Linked[2].Local := CalculateEndPoint(Len,
+        Point(Parts[12, 1], Parts[12, 2]));
+      with Linked[2] do
       begin
         SetLength(Linked, 1);
-        Linked[0].IsFoot := true;
-        Linked[0].Local := CalculateEndPoint(Trunc(Len * 0.5),
-          Point(Parts[14, 1], Parts[14, 2]));
+        Linked[0].Local := CalculateEndPoint(Len,
+          Point(Parts[13, 1], Parts[13, 2]));
+        with Linked[0] do
+        begin
+          SetLength(Linked, 1);
+          Linked[0].IsFoot := true;
+          Linked[0].Local := CalculateEndPoint(Trunc(Len * 0.5),
+            Point(Parts[14, 1], Parts[14, 2]));
+        end;
       end;
     end;
   end;
-
 end;
 
 procedure TCharacter.Paint(const CurrentTime: Cardinal);
 var
-  left, right, mid: Integer;
+  left, right, mid: TChrKeyFrameAdr;
+  Steps: Integer;
 begin
-
-  left := Low(self.KeyFrames) - 1;
-  right := High(self.KeyFrames) + 1;
-  while (left + 1 < right) do
-
+  self.Canvas.Pen.Width := 3;
+  self.Canvas.Pen.Color := clBlack;
+  self.Canvas.Brush.Color := clWhite;
+  left := KeyFrames;
+  right := nil;
+  mid := left;
+  Steps := 0;
+  while mid <> right do
   begin
-    mid := (left + right) div 2;
-    if self.KeyFrames[mid].Time <= CurrentTime then
+    mid := mid^.Adr;
+    Inc(Steps);
+  end;
+  while (left^.Adr <> right) do
+  begin
+    mid := left;
+    for Steps := 1 to Steps div 2 do
+      mid := mid^.Adr;
+    if mid^.Inf.Time <= CurrentTime then
       left := mid
     else
       right := mid;
   end;
-  if right > High(self.KeyFrames) then
-     right := left;
-     
-  Draw(self.KeyFrames[left].Body, self.KeyFrames[right].Body,
-    self.KeyFrames[left].Time, self.KeyFrames[right].Time, CurrentTime,
-    Lerp(self.KeyFrames[left].Body.Local, self.KeyFrames[right].Body.Local,
-    self.KeyFrames[left].Time,CurrentTime, self.KeyFrames[right].Time), self.KeyFrames[left].Len);
+  if right = nil then
+    right := left;
+
+  Draw(left.Inf.Body, right.Inf.Body, left.Inf.Time, right.Inf.Time,
+    CurrentTime, Lerp(left.Inf.Body.Local, right.Inf.Body.Local, left.Inf.Time,
+    CurrentTime, right.Inf.Time), left.Inf.Len);
 end;
 
 function TCharacter.CalculateEndPoint(Length: Integer;
@@ -203,38 +224,38 @@ begin
   Result := Point(Round(deltaX), Round(deltaY));
 end;
 
-procedure TCharacter.Draw(const left, right: TBodyPart;const LeftT, RightT, CurT: Cardinal;
- const CurAbs: TPoint;const Len: Integer);
+procedure TCharacter.Draw(const left, right: TBodyPart;
+  const LeftT, RightT, CurT: Cardinal; const CurAbs: TPoint;
+  const Len: Integer);
 var
   i, R: Integer;
   NextAbsolute, BodyPart: TPoint;
 begin
 
-
-
   for i := 0 to High(left.Linked) do
   begin
-    BodyPart := self.CalculateEndPoint(Len, Lerp(left.Linked[i].Local, right.Linked[i].Local, LeftT,
-      CurT, RightT));
+    BodyPart := self.CalculateEndPoint(Len, Lerp(left.Linked[i].Local,
+      right.Linked[i].Local, LeftT, CurT, RightT));
     if left.Linked[i].IsHead then
     begin
       R := Round(Sqrt(Sqr(BodyPart.X) + Sqr(BodyPart.Y))) * 3 div 4;
-      self.Canvas.Ellipse(CurAbs.X + BodyPart.X * 3 div 4  - R,
-        CurAbs.Y + BodyPart.Y * 3 div 4 + R, CurAbs.X + BodyPart.X * 3 div 4  + R,
-        CurAbs.Y + BodyPart.Y * 3 div 4  - R);
+      self.Canvas.Ellipse(CurAbs.X + BodyPart.X * 3 div 4 - R,
+        CurAbs.Y + BodyPart.Y * 3 div 4 + R, CurAbs.X + BodyPart.X * 3 div 4 +
+        R, CurAbs.Y + BodyPart.Y * 3 div 4 - R);
     end
     else
     begin
       self.Canvas.MoveTo(CurAbs.X, CurAbs.Y);
       if left.Linked[i].IsFoot then
         self.Canvas.LineTo(CurAbs.X + BodyPart.X div 2,
-        CurAbs.Y  + BodyPart.Y div 2)
+          CurAbs.Y + BodyPart.Y div 2)
       else
-        self.Canvas.LineTo(CurAbs.X + BodyPart.X,
-        CurAbs.Y + BodyPart.Y);
+        self.Canvas.LineTo(CurAbs.X + BodyPart.X, CurAbs.Y + BodyPart.Y);
     end;
-      NextAbsolute := Point(CurAbs.X + BodyPart.X, CurAbs.Y + BodyPart.Y);
-    Draw(left.Linked[i], right.Linked[i], LeftT, RightT, CurT, NextAbsolute, Len);
+    NextAbsolute := Point(CurAbs.X + BodyPart.X, CurAbs.Y + BodyPart.Y);
+    Draw(left.Linked[i], right.Linked[i], LeftT, RightT, CurT,
+      NextAbsolute, Len);
   end;
 end;
+
 end.
--- Main.pas
+++ Main.pas
@@ -5,7 +5,9 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
   System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, CharacterController, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, CharacterController, EventController,
+  BackGroundController,
+  Vcl.ExtCtrls;
 
 type
   TForm2 = class(TForm)
@@ -23,22 +25,43 @@ var
 implementation
 
 {$R *.dfm}
-const
-  First:TBodyPRow = ((200,200),(0,-1),(0,-1),(0,-1),(-1,1),(0,1),(1,1),(0,1),(-1,1),(0,1),(-1,0),(1,1),(0,1),(1,0));
-  Second:TBodyPRow = ((300,200),(0,-1),(0,-1),(0,-1),(-1,1),(0,1),(1,-1),(0,-1),(-1,1),(0,1),(-1,0),(1,1),(0,1),(1,0));
-  Secon:TBodyPRow = ((300,200),(0,-1),(0,-1),(0,-1),(-1,1),(0,1),(1,0),(1,0),(-1,1),(0,1),(-1,0),(1,1),(0,1),(1,0));
-  Tri:TBodyPRow = ((300,200),(0,-1),(0,-1),(0,-1),(-1,1),(0,1),(1,1),(0,1),(-1,1),(0,1),(-1,0),(1,1),(0,1),(1,0));
 
+const
+  First: TBodyPRow = ((200, 400), (0, -1), (0, -1), (0, -1), (-1, 1), (0, 1),
+    (1, 1), (0, 1), (-1, 1), (0, 1), (-1, 0), (1, 1), (0, 1), (1, 0));
+  Second: TBodyPRow = ((300, 400), (0, -1), (0, -1), (0, -1), (-1, 1), (0, 1),
+    (1, -1), (0, -1), (-1, 1), (0, 1), (-1, 0), (1, 1), (0, 1), (1, 0));
+  Secon: TBodyPRow = ((300, 400), (0, -1), (0, -1), (0, -1), (-1, 1), (0, 1),
+    (1, 0), (1, 0), (-1, 1), (0, 1), (-1, 0), (1, 1), (0, 1), (1, 0));
+  Tri: TBodyPRow = ((300, 400), (0, -1), (0, -1), (0, -1), (-1, 1), (0, 1),
+    (1, 1), (0, 1), (-1, 1), (0, 1), (-1, 0), (1, 1), (0, 1), (1, 0));
 
 var
   Characer: TCharacter;
-  Timer, Start: Cardinal;
-  Scale :Real;
+  bg: TBackGround;
+  Start: Cardinal;
 
 procedure TForm2.OnCreate(Sender: TObject);
-begin
 
+begin
   Start := GetTickCount;
+
+  bg := TBackGround.Create(Form2.Canvas);
+  bg.AddKeyFrame([TRectangle.Create(0, 200, 500, 490, clBlack, clLime),
+    TRectangle.Create(70, 230, 200, 490, clBlack, clMaroon),
+    TRectangle.Create(300, 250, 450, 400, clBlack, clAqua)], [],
+    [TLine.Create(0, 200, 250, 50, clBlack, clLime), TLine.Create(250, 50, 500,
+    200, clBlack, clLime)], 0);
+  bg.AddKeyFrame([TRectangle.Create(0, 200, 500, 490, clBlack, clRed),
+    TRectangle.Create(70, 230, 200, 490, clBlack, clMaroon),
+    TRectangle.Create(300, 250, 450, 400, clBlack, clAqua)], [],
+    [TLine.Create(0, 200, 250, 50, clBlack, clLime), TLine.Create(250, 50, 500,
+    200, clBlack, clLime)], 3000);
+  bg.AddKeyFrame([TRectangle.Create(0, 200, 500, 490, clBlack, clTeal),
+    TRectangle.Create(70, 230, 200, 490, clBlack, clMaroon),
+    TRectangle.Create(300, 250, 450, 400, clBlack, clAqua)], [],
+    [TLine.Create(0, 200, 250, 50, clBlack, clLime), TLine.Create(250, 50, 500,
+    200, clBlack, clLime)], 5000);
   Characer := TCharacter.Create(Form2.Canvas);
   Characer.AddKeyFrame(First, 50, 0);
   Characer.AddKeyFrame(Secon, 50, 1000);
@@ -53,10 +76,13 @@ begin
 end;
 
 procedure TForm2.OnPaint(Sender: TObject);
+var
+  CurTime: Cardinal;
 begin
-
-  Characer.Paint(GetTickCount - Start);
-  if GetTickCount - Start < 9000 then
+  CurTime := GetTickCount - Start;
+  bg.Paint(CurTime);
+  Characer.Paint(CurTime);
+  if CurTime < 9000 then
     Form2.Invalidate;
 end;
 
